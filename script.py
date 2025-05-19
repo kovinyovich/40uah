6a52b1093d444d1feb01f3abab05dcd244c36eb0
kovinyovich <kovinyovich@icloud.com>
2025-05-19 14:13:39 +0300

    my first commit on github my second commit on github

--- script.py
+++ script.py
@@ -2,3 +2,5 @@ goal = "i wanna write a program for learning hebrew"
 print(goal)
 goal = "also, i wanna create platform (website, app) for psychologists and esotericists"
 print(goal)
+goal = "i wanna build own dark mansion in real life"
+print(goal)
